9a8e1fbf3f8747ca170f1bb804da77c020417d17
Jari63 <56759428+Jari63@users.noreply.github.com>
2020-04-12 20:13:51 +0300

    Removed EF reference from Todo.Api

---
+++ Todo.Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Data;
+
+namespace Todo.Services
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static void AddDataAccessServices(this IServiceCollection services, string connectionString)
+		{
+			services.AddDbContext<TodoContext>(options =>
+			  options.UseSqlServer(connectionString));
+		}
+	}
+}
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -46,7 +45,7 @@ namespace TodoApi
 
 			AddSwaggerDocs(services);
 
-			services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+			services.AddDataAccessServices(Configuration.GetConnectionString("Default"));
 			services.AddTransient<ITodoService, TodoService>();
 		}
 
